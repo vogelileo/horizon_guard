6ab52a37e0b8697a7a83a5a458123940d9fba186
Leo Vogel <Leo.Vogel@ibm.com>
2024-11-28 23:48:02 +0100

    Update intruder-detection.js

--- backend/helpers/intruder-detection.js
+++ backend/helpers/intruder-detection.js
@@ -2,7 +2,7 @@ const intruderRadar = (radar) => {
   return radar === true;
 };
 
-const intruderVibration = (x_old, y_old, z_old, x_new, y__new, z_new) => {
+const intruderVibration = (x_old, y_old, z_old, x_new, y_new, z_new) => {
   let dx = x_new - x_old;
   let dy = y_new - y_old;
   let dz = z_new - z_old;
