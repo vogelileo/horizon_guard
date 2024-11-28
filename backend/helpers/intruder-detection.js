402008f7abfa733780e209393a1691a83a5a4220
Leo Vogel <Leo.Vogel@ibm.com>
2024-11-29 00:45:06 +0100

    Update intruder-detection.js

--- backend/helpers/intruder-detection.js
+++ backend/helpers/intruder-detection.js
@@ -6,7 +6,7 @@ const intruderVibration = (x_old, y_old, z_old, x_new, y_new, z_new) => {
   let dx = x_new - x_old;
   let dy = y_new - y_old;
   let dz = z_new - z_old;
-  return Math.sqrt(dx * dx + dy * dy + dz * dz) > 30;
+  return Math.sqrt(dx * dx + dy * dy + dz * dz) > 15;
 };
 
 const intruderThermal = (thermalImageold, thermalImagenew) => {};
